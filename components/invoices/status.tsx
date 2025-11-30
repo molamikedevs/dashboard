0e4260d79518eca0ff8ca43fec71f659fab64e75
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-11-30 13:40:27 +0400

    Fix import errors.

--- components/invoices/status.tsx
+++ components/invoices/status.tsx
@@ -1,27 +1,26 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import clsx from "clsx";
+import { Check, Clock } from "lucide-react";
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
-      )}
-    >
-      {status === 'pending' ? (
+          "bg-gray-100 text-gray-500": status === "pending",
+          "bg-green-500 text-white": status === "paid",
+        }
+      )}>
+      {status === "pending" ? (
         <>
           Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          <Clock className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === "paid" ? (
         <>
           Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
+          <Check className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
